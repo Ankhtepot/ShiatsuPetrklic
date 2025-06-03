6745854c8b227227c627726d524b56adc02317e1
Zavodny Petr <petr.zavodny@sensearena.com>
2025-06-03 18:05:26 +0200

    wip contact form styling, styled page-not-found

--- src/app/pages/services/services.component.ts
+++ src/app/pages/services/services.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import {CommonModule} from '@angular/common';
 
 @Component({
-  selector: 'app-home',
+  selector: 'app-services',
   imports: [CommonModule],
   templateUrl: './services.component.html',
   styleUrls: ['./services.component.scss'],
