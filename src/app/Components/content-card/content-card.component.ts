e78e83986cba6cfa7c94b471fc3f6b22b7b97584
Zavodny Petr <petr.zavodny@sensearena.com>
2025-07-01 10:32:24 +0200

    wip fixing mobile screen styling tuned in-construction.component

--- src/app/Components/content-card/content-card.component.ts
+++ src/app/Components/content-card/content-card.component.ts
@@ -1,15 +1,26 @@
-import {Component, Input} from '@angular/core';
+import {Component, computed, Input, signal} from '@angular/core';
 import {NgStyle} from '@angular/common';
 
 @Component({
   selector: 'app-content-card',
   standalone: true,
   templateUrl: './content-card.component.html',
-  imports: [
-    NgStyle
-  ],
-  styleUrls: ['./content-card.component.scss']
+  styleUrls: ['./content-card.component.scss'],
+  imports: [NgStyle]
 })
 export class ContentCardComponent {
-  @Input() width: string = '800px';
+  @Input() width: string = '600px'; // Can be '80%', '60%', '500px', etc.
+
+  private screenWidth = signal(window.innerWidth);
+  readonly style = computed(() => this.isMobile() ? {} : { width: this.width });
+  readonly isMobile = signal(window.innerWidth < 768);
+
+  constructor() {
+    window.addEventListener('resize', () => {
+      const newWidth = window.innerWidth;
+      this.screenWidth.set(newWidth);
+      this.isMobile.set(newWidth < 768);
+    });
+  }
+
 }
--- src/app/Components/in-construction/in-construction.component.ts
+++ src/app/Components/in-construction/in-construction.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, signal} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {T} from '../../shared/constants/text.tokens';
 import {TextPipe} from '../../pipes/text.pipe';
@@ -11,6 +11,13 @@ import {TextPipe} from '../../pipes/text.pipe';
   standalone: true
 })
 export class InConstructionComponent {
-
   protected readonly T = T;
+
+  isAnimating = signal(false);
+  isVisible = signal(true);
+
+  onClick(): void {
+    this.isAnimating.set(true);
+    setTimeout(() => this.isVisible.set(false), 500);
+  }
 }
